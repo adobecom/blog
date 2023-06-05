11d5b6406007e1264300e4c8075714140fc06db4
JC Kautzmann <jkautzma@adobe.com>
2023-06-05 17:26:15 -0400

    MWPW-132087 - [Blog2Milo] Banner Block
    - create a Milo based banner block

---
+++ blocks/banner/banner.js
@@ -0,0 +1,58 @@
+import {
+  createOptimizedPicture,
+  normalizeHeadings,
+} from '../../scripts/scripts.js';
+
+export default async function init(block) {
+  const bannerContents = document.createElement('div');
+  bannerContents.classList.add('banner-contents');
+  block.querySelectorAll('a').forEach(async (a) => {
+    if (a && a.href) {
+      // content wrapper
+      const bannerContent = document.createElement('div');
+      bannerContent.classList.add('content-wrapper');
+
+      // get response from the URL
+      const { pathname } = new URL(a);
+      const path = pathname?.replace(/\.html$/, '');
+      if (path) {
+        const response = await fetch(`${path}.plain.html`);
+        if (response.ok) {
+          const responseEl = document.createElement('div');
+          responseEl.innerHTML = await response.text();
+          block.classList.add('is-loaded');
+
+          // creating banner image and text div.
+          const bannerImage = document.createElement('div');
+          const bannerText = document.createElement('div');
+          bannerImage.classList.add('banner-image');
+          bannerText.classList.add('banner-text');
+
+          // banner image content
+          const img = responseEl.querySelector('img');
+          const picture = img.closest('picture');
+          const newPicture = createOptimizedPicture(img.src, img.alt);
+          if (picture) {
+            picture.parentElement.replaceChild(newPicture, picture);
+            bannerImage.append(newPicture);
+          }
+
+          // banner text content
+          normalizeHeadings(responseEl, ['h3']);
+          const link = responseEl.querySelector('a');
+          link.classList.add('cta-link');
+          bannerText.append(responseEl);
+
+          // appending DOM objects
+          bannerContent.append(bannerImage);
+          bannerContent.append(bannerText);
+          bannerContents.append(bannerContent);
+          block.innerHTML = '';
+          block.append(bannerContents);
+        } else {
+          block.remove();
+        }
+      }
+    }
+  });
+}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,7 +43,75 @@ const CONFIG = {
 async function loadLCPImage() {
   const lcpImg = document.querySelector('main img');
   lcpImg?.setAttribute('loading', 'eager');
-};
+}
+
+/**
+ * Normalizes all headings within a container element.
+ * @param {Element} el The container element
+ * @param {[string]]} allowedHeadings The list of allowed headings (h1 ... h6)
+ */
+export function normalizeHeadings(el, allowedHeadings) {
+  const allowed = allowedHeadings.map((h) => h.toLowerCase());
+  el.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((tag) => {
+    const h = tag.tagName.toLowerCase();
+    if (allowed.indexOf(h) === -1) {
+      // current heading is not in the allowed list -> try first to "promote" the heading
+      let level = parseInt(h.charAt(1), 10) - 1;
+      while (allowed.indexOf(`h${level}`) === -1 && level > 0) {
+        level -= 1;
+      }
+      if (level === 0) {
+        // did not find a match -> try to "downgrade" the heading
+        while (allowed.indexOf(`h${level}`) === -1 && level < 7) {
+          level += 1;
+        }
+      }
+      if (level !== 7) {
+        tag.outerHTML = `<h${level}>${tag.textContent}</h${level}>`;
+      }
+    }
+  });
+}
+
+/**
+ * Returns a picture element with webp and fallbacks
+ * @param {string} src The image URL
+ * @param {boolean} eager load image eager
+ * @param {Array} breakpoints breakpoints and corresponding params (eg. width)
+ */
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 400px)', width: '2000' }, { width: '750' }]) {
+  const url = new URL(src, window.location.href);
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+    }
+  });
+
+  return picture;
+}
 
 /*
  * ------------------------------------------------------------
@@ -57,7 +125,7 @@ const miloLibs = setLibs(LIBS);
   const paths = [`${miloLibs}/styles/styles.css`];
   const { getMetadata } = await import(`${miloLibs}/utils/utils.js`);
 
-  if (STYLES) { 
+  if (STYLES) {
     paths.push(...(Array.isArray(STYLES) ? STYLES : [STYLES]));
   }
 
@@ -87,6 +155,7 @@ function decorateFigure() {
       const img = block.querySelector(':scope picture > img');
       img.classList.add('infograph');
     }
+    // eslint-disable-next-line no-unused-expressions
     block.classList.remove('images') || block.classList.remove('image') || block.classList.remove('infograph') || block.classList.remove('infographic');
     block.classList.add('figure');
   });
@@ -95,18 +164,18 @@ function decorateFigure() {
 function decorateVideo() {
   const videoBlocks = document.querySelectorAll('.embed, .video, .animation');
   videoBlocks.forEach((block) => {
-    const link =  block.querySelector(':scope a');
-    const videoCaption =  block.querySelector(':scope p:last-of-type');
+    const link = block.querySelector(':scope a');
+    const videoCaption = block.querySelector(':scope p:last-of-type');
     const url = block.classList.contains('autoplay') || block.classList.contains('animation')
       ? `${link.href}#_autoplay`
       : link.href;
-    const para =  document.createElement('p');
+    const para = document.createElement('p');
 
     link.href = url;
     para.append(link);
-    block.insertAdjacentElement("beforebegin", para);
+    block.insertAdjacentElement('beforebegin', para);
     block.remove();
-    
+
     if (!videoCaption) return;
     videoCaption.classList.add('video-caption');
     para.insertAdjacentElement('afterend', videoCaption);
